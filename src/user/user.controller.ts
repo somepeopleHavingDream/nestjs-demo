e315d31faaaf2ddfff0e3ff3256e07cb92edce90
yangxin <1393003255@qq.com>
2025-04-11 21:52:02 +0800

    feat: nestjs 服务端开发：通用业务框架设计【配置】，官方方案：使用 config 设置配置模块

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
+import { ConfigService } from '@nestjs/config';
+import { ConfigEnum } from 'src/enum/config.enum';
 
 @Controller('user')
 export class UserController {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private configService: ConfigService,
+  ) {}
 
   @Get()
   getUsers(): any {
+    const db = this.configService.get<string>(ConfigEnum.DB);
+    const dbHost = this.configService.get<string>(ConfigEnum.DB_HOST);
+    console.log('DB Host:', dbHost);
+    console.log('DB:', db);
     return this.userService.getUsers();
   }
 }
